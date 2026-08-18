d1e2a23871801faa0c10d01c2e4e20c335c46e84
Ganesh Bhadra <39549618+hunk7@users.noreply.github.com>
2026-08-18 14:40:03 +0530

    Create MinEatingSpeed.cs

---
+++ MinEatingSpeed.cs
@@ -0,0 +1,62 @@
+/* https://leetcode.com/problems/koko-eating-bananas/ | Leetcode #875 - Koko Eating Bananas */
+
+public class Solution
+{
+    public int MinEatingSpeed(int[] piles, int h)
+    {
+        int left = 1, right = piles.Max();
+        while (left < right)
+        {
+            int mid = left + (right - left) / 2;
+            long hours = 0;
+            foreach (int pile in piles) {
+                hours += (pile + mid - 1) / mid;
+            }
+            if (hours <= h) {
+                right = mid;     // try smaller speed
+            } else {
+                left = mid + 1;  // speed too slow
+            }
+        }
+        return left;
+    }
+}
+
+/*
+875. Koko Eating Bananas
+
+Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. The guards have gone and will come back in h hours.
+
+Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas and eats k bananas from that pile.
+If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during this hour.
+
+Koko likes to eat slowly but still wants to finish eating all the bananas before the guards return.
+
+Return the minimum integer k such that she can eat all the bananas within h hours.
+ 
+Example 1:
+
+Input: piles = [3,6,7,11], h = 8
+Output: 4
+Example 2:
+
+Input: piles = [30,11,23,4,20], h = 5
+Output: 30
+Example 3:
+
+Input: piles = [30,11,23,4,20], h = 6
+Output: 23
+ 
+
+Constraints:
+
+1 <= piles.length <= 104
+piles.length <= h <= 109
+1 <= piles[i] <= 109
+
+Interview Answer -
+
+We binary search the eating speed k from 1 to max(piles). For each speed, we calculate total hours using (pile + k - 1) / k.
+If Koko can finish within h hours, we try a smaller speed; otherwise, we increase the speed. This gives O(n log(maxPile)) time and O(1) space.
+
+*/

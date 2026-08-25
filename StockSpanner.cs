799615bbec5f8eb16eb9ba520d7bc0b71b75fcfc
Ganesh Bhadra <39549618+hunk7@users.noreply.github.com>
2026-08-25 18:30:01 +0530

    Create StockSpanner.cs

---
+++ StockSpanner.cs
@@ -0,0 +1,61 @@
+/* https://leetcode.com/problems/online-stock-span/ | Leetcode #901 - Online Stock Span */
+
+public class StockSpanner {
+    private Stack<(int Price, int Span)> stack;
+    public StockSpanner() {
+        stack = new Stack<(int Price, int span)>();
+    }
+    public int Next(int price) {
+        int span = 1;
+        while(stack.Count() > 0 && stack.Peek().Price <= price) {
+            span += stack.Pop().Span;
+        }
+        stack.Push((price,span));
+        return span;
+    }
+}
+
+/*
+901. Online Stock Span
+
+Design an algorithm that collects daily price quotes for some stock and returns the span of that stock's price for the current day.
+
+The span of the stock's price in one day is the maximum number of consecutive days (starting from that day and going backward)
+for which the stock price was less than or equal to the price of that day.
+
+For example, if the prices of the stock in the last four days are [7,2,1,2] and the price of the stock today is 2, then the span of today is 3 because starting from today, the price of the stock was less than or equal to 2 for 3 consecutive days.
+Also, if the prices of the stock in the last four days is [7,34,1,2] and the price of the stock today is 8, then the span of today is 3 because starting from today, the price of the stock was less than or equal 8 for 3 consecutive days.
+Implement the StockSpanner class:
+
+StockSpanner() Initializes the object of the class.
+int next(int price) Returns the span of the stock's price given that today's price is price.
+
+Example 1:
+
+Input
+["StockSpanner", "next", "next", "next", "next", "next", "next", "next"]
+[[], [100], [80], [60], [70], [60], [75], [85]]
+Output
+[null, 1, 1, 1, 2, 1, 4, 6]
+
+Explanation
+StockSpanner stockSpanner = new StockSpanner();
+stockSpanner.next(100); // return 1
+stockSpanner.next(80);  // return 1
+stockSpanner.next(60);  // return 1
+stockSpanner.next(70);  // return 2
+stockSpanner.next(60);  // return 1
+stockSpanner.next(75);  // return 4, because the last 4 prices (including today's price of 75) were less than or equal to today's price.
+stockSpanner.next(85);  // return 6
+ 
+Constraints:
+
+1 <= price <= 105
+At most 104 calls will be made to next
+
+Interview Answer ~
+
+I use a monotonic decreasing stack storing (price, span). For each new price, I initialize span = 1, then pop all prices less than or equal to the current price and add their stored spans to the current span. 
+After that, I push (currentPrice, span) back onto the stack and return the span. This works because popped elements are already covered by the current price. Each element is pushed and popped at most once, giving O(1) amortized time and O(n) space.
+
+*/
